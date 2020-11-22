ff53d120c4ef83079ba5675ba4159505372c80bf
eperezdeach <73988798+eperezdeach@users.noreply.github.com>
2020-11-22 17:42:18 +0100

    Update functions.cpp
    functions entrega_1

--- functions.cpp
+++ functions.cpp
@@ -1,40 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <set>
+#include "functions.h"
+
+
+//Separate line into single strings
+void SeparateWords(std::string word, std::vector<std::string> &vec){
+    std::string tmp;
+    for (auto it = 0; it < word.length(); ++it){
+        //std::cout << word[ii] << std::endl;
+        if((int(word[it])>=32 && int(word[it])!=39 && int(word[it])<=47) || (int(word[it])>=58  && int(word[it])<=64)){
+            if (tmp!=""){vec.push_back(tmp);}
+            tmp = word[it];
+            vec.push_back(tmp);
+            tmp.clear();
+            continue;
+        }
+        tmp += word[it];
+    }
+    vec.push_back(tmp);
+}
+
+//Convert string to set
+void dict2set(char* file, std::set<std::string> &d){
+    std::string ln;
+
+    std::ifstream in(file);
+    if(!in){std::cerr << "ERROR DICTIONARY" ; return;}
+    while(!in.eof()){
+        in >> ln;
+        d.insert(ln);
+    }
+    in.close();
+}//Convert the dictionary into string format
+
+//Add special characters to dictionary - Except \'
+void AddSpecChar2Dict(std::set<std::string> &d) {
+    std::string tmp_i;
+    std::string tmp_j;
+    int ii= 32, jj = 58;
+    while(ii < 48 || jj < 65){
+        if(ii<48){
+            tmp_i.push_back((char)ii);
+            d.insert(tmp_i);
+        }
+        if(jj<65){
+            tmp_j.push_back((char)jj);
+            d.insert(tmp_j);
+        }
+        tmp_i.clear();
+        tmp_j.clear();
+        ++ii;++jj;
+    }
+
+}
 
 /*Cap2Low: convert the word's letter from capital letter to lower case
  * INPUT: std::string -> STRING THAT WILL BE CONVERTED TO LOWER CASE
  * OUTPUT: std::string <- RETURN THE STRING IN LOWER CASE
  */
-
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <thread>
-#include <vector>
-#include "functions.h"
-#include "class.h"
-
-
-void dict2str(std::string &d){
-    std::string ln;
-
-    std::ifstream in("words_alpha.txt");
-    if(!in){std::cerr << "ERROR DICTIONARY" ; return;}
-    while(!in.eof()){
-        in >> ln;
-        d += ln + " ";
-    }
-    in.close();
-}//Convert the dictionary into string format
-
-void wrds_line(const std::string ln, std::vector<std::string>& v){
-    std::stringstream check(ln);
-    std::string word;
-    while(getline(check,word,' ')){
-         v.push_back(word); //add word to vec
-    }
-    v.emplace_back("<br>\n");
-}//Break the line into words
-
 std::string Cap2Low(std::string wrd){
     std::string tmp;
     tmp=wrd;
@@ -46,26 +74,6 @@ std::string Cap2Low(std::string wrd){
     return tmp;
 }
 
-/* DelSpecChar: Delete Special Characters
- * INPUT: std::string -> STRING THAT WILL ANALYSED FOR SPECIAL CHARACTERS
- * OUTPUT: std::string <- RETURN THE STRING WITHOUT SPEC. CHAR.
- */
-std::string DelSpecChar(std::string wrd) {
-    std::string tmp = wrd; //tmp: temporary string
-    int len = tmp.length(); //length of tmp
-    if(int(tmp[len-1])==39){tmp.erase(len-1);}//delete \' at the end
-    for (auto it=tmp.cbegin(); it!=tmp.cend();++it){ //loop tmp chars
-        //check if the are special chars
-        if((int(*it)>=33 && int(*it)!=39 && int(*it)<=47) || (int(*it)>=58  && int(*it)<=64)){
-            tmp.erase(it); //delete special char
-            --it;
-        }
-    }
-
-    //if (tmp.length()==0){tmp=wrd;}//if the word is all spec chars, return wrd
-    return tmp;
-}
-
 /* IsNum: Check if the word is a number
  * INPUT: std::string -> STRING THAT WILL ANALYSED IF IS A NUMBER
  * OUTPUT: bool -> RETURNS IF THE WORD IS ALL NUMBERS
@@ -73,8 +81,9 @@ std::string DelSpecChar(std::string wrd) {
 bool IsNum (const std::string wrd)
 {
     int l=wrd.length();
+    std::string tmp = wrd;
     bool num=true;
-    for (auto it = wrd.cbegin(); it != wrd.end();++it){ //iterate wrd's chars
+    for (auto it = tmp.cbegin(); it != tmp.end();++it){ //iterate wrd's chars
         if (int(*it)< 48 || int(*it) > 57 ) //Check if it is not a number
         {
             num = false; //no es num
@@ -93,28 +102,29 @@ bool IsNum (const std::string wrd)
 std::string ContraccionOK (std::string word)
 {
     //Separo pre y post apostrofe
-    std::vector <std::string> token; //token: vector that saves the word and contraction
+    std::vector <std::string> token = {"",""}; //token: vector that saves the word and contraction
     std::string tmp = word; //tmp: temporary string
-    std::stringstream check(word);
+    std::stringstream check(tmp);
+    std::string contr = "";
+    std::string wrd = "";
     std::string intermediate;//intermediate: word or contr
+    int pos = 0;
     while (getline(check, intermediate,'\'')){//split words by the \'
-        token.push_back(intermediate); //add intermediate to token
+        token[pos]=intermediate; //add intermediate to token
+        ++pos;
     }
     //asigno palabra y contraccion
-    std::string contr=token [1]; //contr: contraction of word
-    std::string wrd=token[0]; //wrd: word without contraction
+    contr= token [1]; //contr: contraction of word
+    wrd=token[0]; //wrd: word without contraction
 
-    bool contrOK = false;//variable mia interna de control, 0 mal 1 bien
     //Caso 1: apostrofe colocado en pos incorrecta --> PALABRA MAL
-    if (contr.length() > 2) std::cout <<"Incorrect \n";
+    if (contr.length() > 2) std::cout <<"Incorrect: " << tmp << "\n";
         //Caso 2: apostrofe en antepenúltima posicion.
     else if (contr.length() == 2){
         std::vector <std::string> lst_2 = {"re","ll","ve"};
         for (const auto& c: lst_2)
         {
             if (c == contr) {
-                //std::cout <<"Its the contr --> " << c;
-                contrOK=true;
                 tmp.empty();
                 tmp=wrd;
                 break;
@@ -128,8 +138,6 @@ std::string ContraccionOK (std::string word)
         for (auto c: lst_1)
         {
             if (c == contr) {
-                //std::cout <<"Its the contr --> " << c;
-                contrOK=true;
                 tmp.empty();
                 tmp=wrd;
                 if(tmp[tmp.length()-1]=='n' && c =="t"){tmp.erase(tmp.length()-1);}
@@ -138,11 +146,12 @@ std::string ContraccionOK (std::string word)
         }
         //si control OK es uno, habría que comprobar que la palabra wrd esta en dic
     }else if (contr.length()==0){
-        contrOK=true;
         tmp.empty();
         tmp=wrd;
     }
     //Caso 4: apostrofe es la ultima palabra. En este caso ir directamente a buscar wrd.
+    if(wrd[wrd.length()-1]=='s'){tmp=wrd;}
+    token.clear();
     return tmp;
 }
 
@@ -166,46 +175,63 @@ bool WordContr (const std::string word){
     return contr;
 }
 
-//FUNCIONES DE HYPHEN
-/*bool FindHyphen (std::string word)
-{
-    for (int i = 0; i < word.length() - 1; i ++ )
-    {
-        if (word[i] == (char) '-')
-        {
-            return true;
-            break;
+//Read input file
+void Read(const char* file,std::vector<std::string> &ln_v,std::vector<std::string> &wrds_v){
+    std::ifstream in(file);
+    std::string line;
+    bool found;
+    //std::cout << "Opening file...\n";
+
+    std::unique_lock<std::mutex> lock(mut);
+    while(getline(in,line)){
+        wrds_v.clear();
+        SeparateWords(line, wrds_v);
+        bool f, c;
+        std::string tmp;//tmp: temporal string
+        std::string tmp_line;//tmp: temporal
+        for (auto w: wrds_v){
+            found = dict.find(w) != dict.end();
+            if(!found) {
+                tmp=w;//copy w in tmp
+                found=IsNum(tmp);
+                tmp=Cap2Low(tmp);//convert to lower cases
+                c=WordContr(tmp);
+                if (c){
+                    tmp=ContraccionOK(tmp);
+                }
+                if(!found){found = dict.find(tmp)!= dict.end();}
+            }
+            if(!found){
+                w = "<a style=\"color:red\">" + w + "</a>";
+            }
+            tmp_line+=w;
         }
+        if (tmp_line!=line){
+            line = tmp_line;
+        }
+        ln_v.push_back(line);
+        tmp_line.clear();
+        tmp.clear();
+        data_cond.notify_one();
     }
-    return false;
 }
 
-std::string GuionPre (std::string word) {
-    //Separo pre y post guion
-    std::vector<std::string> token; //token: vector that saves the word and contraction
-    std::string tmp = word; //tmp: temporary string
-    std::stringstream check(word);
-    std::string intermediate;//intermediate: word or contr
-    while (getline(check, intermediate, '\'')) {//split words by the \'
-        token.push_back(intermediate); //add intermediate to token
+void Write(std::vector<std::string> &v){
+
+    std::unique_lock<std::mutex> lock(mut);
+    data_cond.wait(lock);
+    std::this_thread::sleep_for(10ms);
+    std::ofstream out("output.html");
+    std::cout << "Writing file...\n";
+    out << "<html>\n";
+    out << "\t<body>\n";
+
+
+    for (auto w:v){
+        out << w << "<br>\n";
     }
-    //asigno palabra y contraccion
-    //std::string wrd2 = token[1]; //contr: contraction of word
-    std::string wrd1 = token[0]; //wrd: word without contraction
-    return wrd1;
+    out << "\t</body>\n";
+    out << "</html>\n";
+    out.close();
 }
 
-std::string GuionPost (std::string word) {
-    //Separo pre y post guion
-    std::vector<std::string> token; //token: vector that saves the word and contraction
-    std::string tmp = word; //tmp: temporary string
-    std::stringstream check(word);
-    std::string intermediate;//intermediate: word or contr
-    while (getline(check, intermediate, '\'')) {//split words by the \'
-        token.push_back(intermediate); //add intermediate to token
-    }
-    //asigno palabra y contraccion
-    std::string wrd2 = token[1]; //contr: contraction of word
-    //std::string wrd1 = token[0]; //wrd: word without contraction
-    return wrd2;
-}*/
